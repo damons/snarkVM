8955b30f10a2691758ba2932d262bdbbcaacaeae
Niklas Long <niklas@long.ch>
2025-01-22 14:27:07 +0100

    feat: introduce a `BATCH_SPEND_LIMIT`

--- console/network/src/lib.rs
+++ console/network/src/lib.rs
@@ -132,6 +132,9 @@ pub trait Network:
     const MAX_FEE: u64 = 1_000_000_000_000_000;
     /// The maximum number of microcredits that can be spent on a finalize block.
     const TRANSACTION_SPEND_LIMIT: u64 = 100_000_000;
+    /// The maximum number of microcredits that can be spent by the transactions in a batch.
+    /// This implies the block spend limit is bounded at `TRANSACTION_SPEND_LIMIT * N::NUM_MAX_CERTIFICATES`.
+    const BATCH_SPEND_LIMIT: u64 = Self::TRANSACTION_SPEND_LIMIT;
     /// The base cost in microcredits to verify an execution.
     /// NOTE: this constant reflects the compute cost of an execution, but is not required to be paid by the user.
     const EXECUTION_BASE_COST: u64 = 2_000_000; // 2 million microcredits.
