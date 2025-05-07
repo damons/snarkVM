d6036666c7f4bcaf0b5fd20d05a311f3cacbb440
raychu86 <14917648+raychu86@users.noreply.github.com>
2025-05-07 10:33:23 -0400

    Update parameters url

--- parameters/src/testnet/mod.rs
+++ parameters/src/testnet/mod.rs
@@ -19,7 +19,7 @@ pub use genesis::*;
 /// The restrictions list as a JSON-compatible string.
 pub const RESTRICTIONS_LIST: &str = include_str!("./resources/restrictions.json");
 
-const REMOTE_URL: &str = "https://s3.us-west-1.amazonaws.com/testnet.parameters";
+const REMOTE_URL: &str = "https://parameters.provable.com/testnet";
 
 // BondPublic
 impl_remote!(BondPublicProver, REMOTE_URL, "resources/", "bond_public", "prover");
