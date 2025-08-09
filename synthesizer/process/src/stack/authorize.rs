6905f2a92db93e1cb9962554f75047f5f42f22a5
vicsn <victor.s.nicolaas@protonmail.com>
2025-08-09 13:01:54 +0200

    Update authorize_request comment

--- synthesizer/process/src/stack/authorize.rs
+++ synthesizer/process/src/stack/authorize.rs
@@ -127,7 +127,7 @@ impl<N: Network> Stack<N> {
         Ok(authorization)
     }
 
-    /// Authorizes calls to public credits.aleo functions for the given request.
+    /// Authorizes a call to a public functions for the given request.
     /// Compared to `authorize`, no private key is needed, but this only works for single public requests.
     #[inline]
     pub fn authorize_request<A: circuit::Aleo<Network = N>, R: Rng + CryptoRng>(
