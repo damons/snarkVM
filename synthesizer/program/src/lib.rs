655883a294dfbd2006af95d8d105e8df33cfbe7b
raychu86 <14917648+raychu86@users.noreply.github.com>
2025-10-30 15:32:09 -0700

    cleanup

--- synthesizer/program/src/lib.rs
+++ synthesizer/program/src/lib.rs
@@ -913,22 +913,6 @@ impl<N: Network> ProgramCore<N> {
         false
     }
 
-    /// Returns `true` if a program contains any V12 syntax.
-    /// This includes `Operand::BlockTimestamp`.
-    /// This is enforced to be `false` for programs before `ConsensusVersion::V12`.
-    #[inline]
-    pub fn contains_v12_syntax(&self) -> bool {
-        // Check each instruction and output in each function's finalize scope for the use of
-        // `Operand::BlockTimestamp`.
-        cfg_iter!(self.functions()).any(|(_, function)| {
-            function.finalize_logic().is_some_and(|finalize_logic| {
-                cfg_iter!(finalize_logic.commands()).any(|command| {
-                    cfg_iter!(command.operands()).any(|operand| matches!(operand, Operand::BlockTimestamp))
-                })
-            })
-        })
-    }
-
     /// Returns `true` if the program contains an array type with a size that exceeds the given maximum.
     pub fn exceeds_max_array_size(&self, max_array_size: u32) -> bool {
         self.mappings.values().any(|mapping| mapping.exceeds_max_array_size(max_array_size))
@@ -981,6 +965,22 @@ impl<N: Network> ProgramCore<N> {
         let array_size_exceeds = self.exceeds_max_array_size(V10_MAX_ARRAY_ELEMENTS);
 
         function_contains || closure_contains || command_contains || array_size_exceeds
+    }
+
+    /// Returns `true` if a program contains any V12 syntax.
+    /// This includes `Operand::BlockTimestamp`.
+    /// This is enforced to be `false` for programs before `ConsensusVersion::V12`.
+    #[inline]
+    pub fn contains_v12_syntax(&self) -> bool {
+        // Check each instruction and output in each function's finalize scope for the use of
+        // `Operand::BlockTimestamp`.
+        cfg_iter!(self.functions()).any(|(_, function)| {
+            function.finalize_logic().is_some_and(|finalize_logic| {
+                cfg_iter!(finalize_logic.commands()).any(|command| {
+                    cfg_iter!(command.operands()).any(|operand| matches!(operand, Operand::BlockTimestamp))
+                })
+            })
+        })
     }
 }
 
