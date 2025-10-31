b04dc9b0cf07c3e00149efc92f3e15891da7948b
ljedrz <ljedrz@users.noreply.github.com>
2025-10-31 15:17:03 +0100

    Revert "feat: improve the panic message if run_sequential_operation is called from async"
    This reverts commit f53027c7de671122e1603eaf0da4f442f1003df0.

--- synthesizer/src/vm/helpers/sequential_op.rs
+++ synthesizer/src/vm/helpers/sequential_op.rs
@@ -58,14 +58,6 @@ impl<N: Network, C: ConsensusStorage<N>> VM<N, C> {
 
     /// Sends the given operation to the thread used for sequential processing.
     pub fn run_sequential_operation(&self, op: SequentialOperation<N>) -> Option<SequentialOperationResult<N>> {
-        // Ensure that this method isn't being called from an async context.
-        if tokio::runtime::Handle::try_current().is_ok() {
-            panic!(
-                "Logic bug: a sequential VM operation was attempted in an async \
-                 context; it needs to be wrapped in a blocking task instead."
-            );
-        }
-
         debug!("Queuing operation '{op}' for sequential processing");
 
         // Remember if this is a shutdown.
@@ -85,8 +77,9 @@ impl<N: Network, C: ConsensusStorage<N>> VM<N, C> {
                 return None;
             }
 
-            // Wait for the result of the queued operation. This is a blocking
-            // operation, and would panic in async contexts.
+            // Wait for the result of the queued operation. This is a blocking method,
+            // and will panic in async contexts (which doesn't happen in production, as
+            // we already perform all these operations within blocking tasks).
             let Ok(response) = response_rx.blocking_recv() else {
                 return None;
             };
