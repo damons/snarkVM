ff65327f53f3b123c445db819b26237753825a2e
raychu86 <14917648+raychu86@users.noreply.github.com>
2025-11-06 22:50:27 -0500

    Cleanup ordering

--- synthesizer/process/src/stack/finalize_types/initialize.rs
+++ synthesizer/process/src/stack/finalize_types/initialize.rs
@@ -717,6 +717,8 @@ impl<N: Network> FinalizeTypes<N> {
                 bail!("Fatal error: Cannot check command '{opcode}' as an instruction.")
             }
             Opcode::Commit(opcode) => RegisterTypes::check_commit_opcode(opcode, instruction)?,
+            Opcode::Deserialize(opcode) => RegisterTypes::check_deserialize_opcode(opcode, instruction)?,
+            Opcode::ECDSA(opcode) => RegisterTypes::check_ecdsa_opcode(opcode, instruction)?,
             Opcode::Hash(opcode) => RegisterTypes::check_hash_opcode(opcode, instruction)?,
             Opcode::Is(opcode) => match opcode {
                 "is.eq" => ensure!(
@@ -729,6 +731,7 @@ impl<N: Network> FinalizeTypes<N> {
                 ),
                 _ => bail!("Instruction '{instruction}' is not for opcode '{opcode}'."),
             },
+            Opcode::Serialize(opcode) => RegisterTypes::check_serialize_opcode(opcode, instruction)?,
             Opcode::Sign(_) => {
                 // Ensure the instruction has one destination register.
                 ensure!(
@@ -736,9 +739,6 @@ impl<N: Network> FinalizeTypes<N> {
                     "Instruction '{instruction}' has multiple destinations."
                 );
             }
-            Opcode::ECDSA(opcode) => RegisterTypes::check_ecdsa_opcode(opcode, instruction)?,
-            Opcode::Serialize(opcode) => RegisterTypes::check_serialize_opcode(opcode, instruction)?,
-            Opcode::Deserialize(opcode) => RegisterTypes::check_deserialize_opcode(opcode, instruction)?,
             Opcode::Snark(_) => {
                 // Ensure the instruction has one destination register.
                 ensure!(
