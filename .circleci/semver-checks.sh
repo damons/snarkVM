3ca8bcc20327c457c778027a785286fa903a810f
Kai Mast <kai@provable.com>
2025-11-11 17:56:03 -0800

    ci: update baseline rev for semver checks

--- .circleci/semver-checks.sh
+++ .circleci/semver-checks.sh
@@ -3,7 +3,7 @@
 # Ensure that the command is installed.
 cargo install cargo-semver-checks@0.43.0 --locked
 
-BASELINE_REV=13cf4d7 # UPDATE ME ON NECESSARY BREAKING CHANGES
+BASELINE_REV=7a6475c36 # UPDATE ME ON NECESSARY BREAKING CHANGES
 
 # Exclude CLI as it has been removed
 cargo semver-checks --workspace --default-features \
