0b7cea43e74ad812a44fe4e5045f4b4309398fff
Kai Mast <kai@provable.com>
2025-11-13 15:06:21 -0800

    ci: bump baseline revision for semver checks

--- .circleci/semver-checks.sh
+++ .circleci/semver-checks.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
+set -x
+
 # Ensure that the command is installed.
 cargo install cargo-semver-checks@0.43.0 --locked
 
-BASELINE_REV=dec54170ce # UPDATE ME ON NECESSARY BREAKING CHANGES
+BASELINE_REV=5eb528f # UPDATE ME ON NECESSARY BREAKING CHANGES
 
 # Exclude CLI as it has been removed
-cargo semver-checks --workspace --default-features \
-  --exclude=snarkvm-cli --baseline-rev $BASELINE_REV
+cargo semver-checks --workspace --default-features --baseline-rev $BASELINE_REV
